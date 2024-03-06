933ddedf28fbae4b0f766a04233e78bbe52dc4d3
vu minh tu <vuv909@gmail.com>
2024-03-06 18:45:05 +0700

    code

--- fe/src/component/Home/index.tsx
+++ fe/src/component/Home/index.tsx
@@ -1,55 +1,47 @@
 import { useEffect, useRef } from "react";
-import { useSearchParams } from "react-router-dom";
-import Navbar from "../Navbar";
-import ListOfProduct from "../ListOfProducts";
-import { getProducts } from "../../services/product.api";
-import { LIMIT } from "../../constant/index,";
+import { Navbar } from "../Navbar";
+import { ListOfProduct } from "../ListOfProducts";
 import { useAppDispatch } from "../../hooks/hooks/hook";
 import { ProductAction } from "../../redux/actions/product.action";
+import { useLocation } from "react-router-dom";
+import { getProducts } from "../../services/product.api";
+import { LIMIT } from "../../constant/index,";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 
 function Home() {
   const ref = useRef<HTMLInputElement | null>(null);
-  const [params, setParams] = useSearchParams();
   const dispatch = useAppDispatch();
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+
+  const { data, isLoading, isError } = useQuery(["products", params], () =>
+    fetchProducts(params)
+  );
+
+  const fetchProducts = async (params: URLSearchParams) => {
+    const paramsObject = Object.fromEntries(params.entries());
+    const pageNumber = Number(paramsObject.page);
+    const res = await getProducts({
+      title_like: paramsObject.title,
+      _page: pageNumber,
+      _limit: LIMIT,
+    });
+    return res.data;
+  };
 
   useEffect(() => {
-    const paramsObject = Object.fromEntries(params.entries());
-    console.log(paramsObject);
-    if (paramsObject.title && paramsObject.page) {
-      const pageNumber = Number(paramsObject.page);
-
-      if (!isNaN(pageNumber)) {
-        getProducts({
-          title_like: paramsObject.title,
-          _page: pageNumber,
-          _limit: LIMIT,
-        }).then((res) => {
-          dispatch(
-            ProductAction(
-              res.data.data,
-              false,
-              res?.data?.pagination?._totalRows / res?.data?.pagination?._limit,
-              res?.data?.pagination?._page
-            )
-          );
-        });
-      }
-    } else if (paramsObject.title) {
-      getProducts({
-        title_like: paramsObject.title,
-        _limit: LIMIT,
-      }).then((res) => {
-        dispatch(
-          ProductAction(
-            res.data.data,
-            false,
-            res?.data?.pagination?._totalRows / res?.data?.pagination?._limit,
-            res?.data?.pagination?._page
-          )
-        );
-      });
+    if (!isLoading && !isError && data) {
+      const { pagination, data: productsData } = data;
+      dispatch(
+        ProductAction(
+          productsData,
+          false,
+          pagination._totalRows / pagination._limit,
+          pagination._page
+        )
+      );
     }
-  }, []);
+  }, [data, isLoading, isError]);
 
   return (
     <div className="bg-gray-100">
@@ -59,4 +51,12 @@ function Home() {
   );
 }
 
-export default Home;
+const queryClient = new QueryClient();
+
+const HomeWithQueryClient = () => (
+  <QueryClientProvider client={queryClient}>
+    <Home />
+  </QueryClientProvider>
+);
+
+export default HomeWithQueryClient;
